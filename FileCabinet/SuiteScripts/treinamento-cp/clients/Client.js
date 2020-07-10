9d365f11631e2b70c7c1e6246f3f866402957aee
Volnei Ferreira <volnei.ferreira@outlook.com>
2020-07-10 19:35:59 -0300

    First class: Client Script.

---
+++ FileCabinet/SuiteScripts/treinamento-cp/clients/Client.js
@@ -0,0 +1,217 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/ui/dialog', '../third/vanilla-masker.min.js', 'N/currentRecord'],
+  function (dialog, VMasker, currentRecord) {
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.mode - The mode in which the record is being accessed (create, copy, or edit)
+     */
+    function pageInit (context) {
+      // debugger;
+      // alert('mode: ' + mode);
+      // console.log('mode: ' + mode);
+      VMasker(document.querySelector('#phone')).maskPattern('(99) 9999-9999');
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     * @param {string} context.fieldId - Field name
+     * @param {number} context.line - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} context.column - Line number. Will be undefined if not a matrix field
+     */
+    function fieldChanged (context) {
+      const currentRecord = context.currentRecord
+      const fieldId = context.fieldId
+      const sublistId = context.sublistId
+
+      // var valor = ''
+      //
+      // if (sublistId) {
+      //   valor = currentRecord.getCurrentSublistValue({
+      //     sublistId: sublistId,
+      //     fieldId: fieldId
+      //   });
+      // } else {
+      //   valor = currentRecord.getValue({ fieldId: fieldId })
+      // }
+      //
+      // console.log(
+      //   'sublist: ' + context.sublistId +
+      //   ' linha: ' + context.line +
+      //   ' campo: ' + fieldId + ' valor: ' + valor);
+
+      if (fieldId === 'firstname') {
+        const firstName = currentRecord.getValue({ fieldId: fieldId })
+
+        if (firstName) {
+          currentRecord.setValue({ fieldId: 'phone', value: '9999999999', ignoreFieldChange: true })
+        }
+      }
+
+      if (fieldId === 'entity') {
+        console.log('sub no fieldchanged ' + currentRecord.getValue({ fieldId: 'subsidiary' }))
+      }
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     * @param {string} context.fieldId - Field name
+     */
+    function postSourcing(context) {
+      const currentRecord = context.currentRecord
+      const fieldId = context.fieldId
+
+      if (fieldId === 'entity') {
+        console.log('sub no post ' + currentRecord.getValue({ fieldId: 'subsidiary' }))
+      }
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     */
+    function sublistChanged(context) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     */
+    function lineInit(context) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     * @param {string} context.fieldId - Field name
+     * @param {number} context.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} context.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     */
+    function validateField(context) {
+      const currentRecord = context.currentRecord
+      const fieldId = context.fieldId
+
+      if (fieldId === 'firstname') {
+        const firstName = currentRecord.getValue({ fieldId: fieldId })
+
+        if (firstName.length < 10) {
+          // alert('nome menor que 10');
+          // return false;
+        }
+      }
+
+      return true;
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     */
+    function validateLine(context) {
+
+      return true;
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     */
+    function validateInsert(context) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @param {string} context.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     */
+    function validateDelete(context) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} context
+     * @param {Record} context.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     */
+    function saveRecord(context) {
+      const currentRecord = context.currentRecord
+      const firstName = currentRecord.getValue({ fieldId: 'firstname' })
+
+      if (firstName.length < 10) {
+        dialog.alert({
+          title: 'I am an Alert',
+          message: 'nome menor que 10.'
+        })
+        return false;
+      }
+
+      return true;
+    }
+
+    function myExecute () {
+      const employee = currentRecord.get()
+
+      dialog.alert({
+        title: 'I am an Alert',
+        message: 'Função execute.' + employee.getValue({ fieldId: 'firstname' })
+      })
+    }
+
+    return {
+      pageInit: pageInit,
+      fieldChanged: fieldChanged,
+      postSourcing: postSourcing,
+      sublistChanged: sublistChanged,
+      lineInit: lineInit,
+      validateField: validateField,
+      // validateLine: validateLine,
+      // validateInsert: validateInsert,
+      // validateDelete: validateDelete,
+      saveRecord: saveRecord,
+      myExecute: myExecute
+    }
+  })
